d4b9dc42a6998ebfd028863bea15fe6368586836
cellaaudi <cellaaudi27@gmail.com>
2023-05-24 03:47:24 +0700

    profile ui

--- app/src/main/java/com/cellaaudi/onnea/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/cellaaudi/onnea/ui/profile/ProfileFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import com.cellaaudi.onnea.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -36,6 +37,11 @@ class ProfileFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        (requireActivity() as AppCompatActivity).supportActionBar?.hide()
     }
 
     companion object {
