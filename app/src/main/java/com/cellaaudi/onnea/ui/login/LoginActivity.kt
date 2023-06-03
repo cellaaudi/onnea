ac9ddeef5f9056549d775d78ef99e527f319e6a7
cellaaudi <cellaaudi27@gmail.com>
2023-06-03 16:05:29 +0700

    register button

--- app/src/main/java/com/cellaaudi/onnea/ui/login/LoginActivity.kt
+++ app/src/main/java/com/cellaaudi/onnea/ui/login/LoginActivity.kt
@@ -3,10 +3,16 @@ package com.cellaaudi.onnea.ui.login
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.cellaaudi.onnea.R
+import com.cellaaudi.onnea.databinding.ActivityLoginBinding
+import com.cellaaudi.onnea.databinding.ActivityMainBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
