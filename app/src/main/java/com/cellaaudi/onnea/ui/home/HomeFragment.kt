20e6261833048417b0bb3bfb74c84101706dbffe
cellaaudi <cellaaudi27@gmail.com>
2023-06-16 14:39:53 +0700

    calculate nutrition taken

--- app/src/main/java/com/cellaaudi/onnea/ui/home/HomeFragment.kt
+++ app/src/main/java/com/cellaaudi/onnea/ui/home/HomeFragment.kt
@@ -61,9 +61,8 @@ class HomeFragment : Fragment() {
             binding.txtDay.text = dateFormat.format(selDate)
         }
 
-//        if (uid != null) {
-//            getNutrition(uid)
-//        }
+        getNutrition(uid)
+        calculateNutrition(uid, binding.txtDay.text.toString())
 
         return root
     }
@@ -71,11 +70,7 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val uid = auth.currentUser?.uid
-
-        if (uid != null) {
-            getRecommendation(uid, binding.txtDay.text.toString())
-        }
+        getRecommendation(auth.currentUser?.uid, binding.txtDay.text.toString())
 
         binding.btnPrev.setOnClickListener {
             val button = "prev"
@@ -133,12 +128,8 @@ class HomeFragment : Fragment() {
             }
 
             override fun afterTextChanged(s: Editable?) {
-                showLoading()
-                val uid = auth.currentUser?.uid
-
-                if (uid != null) {
-                    getRecommendation(uid, s.toString())
-                }
+                getRecommendation(auth.currentUser?.uid, s.toString())
+                calculateNutrition(auth.currentUser?.uid, s.toString())
             }
         })
 
@@ -164,12 +155,6 @@ class HomeFragment : Fragment() {
 
         binding.btnAddDinnerR.setOnClickListener {
             addRecommended(auth.currentUser?.uid, binding.txtDay.text.toString(), "Dinner")
-//            val type = "Dinner"
-//            val uid = auth.currentUser?.uid
-//
-//            if (uid != null) {
-//                updateEaten(uid, binding.txtDay.text.toString(), type)
-//            }
         }
     }
 
@@ -244,171 +229,193 @@ class HomeFragment : Fragment() {
         }
     }
 
-//    private fun getNutrition(id: String) {
-//        viewModel.getNutrition(id)
-//
-//        viewModel.nutrition.observe(viewLifecycleOwner) { nutrition ->
-//            if (nutrition != null) {
-//                if (bLoaded && lLoaded && dLoaded) {
-//                    binding.txtTakenCal.text = calTaken.toString()
-//                    binding.txtTakenCarb.text = carbTaken.toString()
-//                    binding.txtTakenProt.text = protTaken.toString()
-//                    binding.txtTakenFat.text = fatTaken.toString()
-//                }
-//
+    private fun showLoadingN() {
+        viewModel.isLoadingN.observe(requireActivity()) { load ->
+            binding.pbNutrition.visibility = if (load) View.VISIBLE else View.GONE
+        }
+    }
+
+    private fun getNutrition(id: String?) {
+        if (id != null) {
+            showLoadingN()
+
+            viewModel.getNutrition(id)
+
+            viewModel.nutrition.observe(viewLifecycleOwner) { nutrition ->
+                if (nutrition != null) {
 //                val calNeeded = str2Int(nutrition.calories)
 //                val carbNeeded = str2Int(nutrition.carbohydrates)
 //                val protNeeded = str2Int(nutrition.protein)
 //                val fatNeeded = str2Int(nutrition.fat)
-//
-//                binding.txtNeededCal.text = " / $calNeeded kcal"
-//                binding.txtNeededCarb.text = " / $carbNeeded g"
-//                binding.txtNeededProt.text = " / $protNeeded g"
-//                binding.txtNeededFat.text = " / $fatNeeded g"
-//            }
-//        }
-//
-//        viewModel.isLoadingN.observe(requireActivity()) {
-//            binding.pbNutrition.visibility = if (it) View.VISIBLE else View.GONE
-//        }
-//    }
 
-    private fun getRecommendation(id: String, date: String) {
-        val (day, month) = getDateMonthNum(date)
-
-        showLoading()
-
-        viewModel.getRecommendation(id, day.toString(), month.toString())
-
-        viewModel.food.observe(viewLifecycleOwner) { recommendation ->
-            if (recommendation.breakfast != null) {
-                val cal = str2Int(recommendation.breakfast[4].calories)
-                val carb = str2Int(recommendation.breakfast[7].carbohydrates)
-                val prot = str2Int(recommendation.breakfast[5].protein)
-                val fat = str2Int(recommendation.breakfast[6].fat)
-
-                Glide.with(requireContext())
-                    .load(recommendation.breakfast[2].link)
-                    .into(binding.imgBreakR)
-                binding.txtBreakNameR.text = recommendation.breakfast[1].name
-                binding.txtBreakCalR.text = "$cal kcal"
-
-                if (recommendation.breakfast[8].eat) {
-//                    calTaken += cal
-//                    carbTaken += carb
-//                    protTaken += prot
-//                    fatTaken += fat
-
-                    Glide.with(requireContext())
-                        .load(recommendation.breakfast[2].link)
-                        .into(binding.imgBreak)
-                    binding.txtBreakName.text = recommendation.breakfast[1].name
-                    binding.txtBreakCal.text = "$cal kcal"
-                    binding.btnAddBreakfast.setImageResource(R.drawable.ic_round_edit_24)
-
-                    binding.cardBreakfastR.visibility = View.GONE
-                } else {
-                    Glide.with(requireContext()).clear(binding.imgBreak)
-                    binding.txtBreakName.text = getString(R.string.food_name)
-                    binding.txtBreakCal.text = getString(R.string.kcal)
-                    binding.btnAddBreakfast.setImageResource(R.drawable.ic_round_add_24)
-
-                    binding.cardBreakfastR.visibility = View.VISIBLE
+                    binding.txtNeededCal.text = " / ${str2Int(nutrition.calories)} kcal"
+                    binding.txtNeededCarb.text = " / ${str2Int(nutrition.carbohydrates)} g"
+                    binding.txtNeededProt.text = " / ${str2Int(nutrition.protein)} g"
+                    binding.txtNeededFat.text = " / ${str2Int(nutrition.fat)} g"
                 }
-
-//                bLoaded = true
             }
-
-            if (recommendation.lunch != null) {
-                val cal = str2Int(recommendation.lunch[4].calories)
-                val carb = str2Int(recommendation.lunch[7].carbohydrates)
-                val prot = str2Int(recommendation.lunch[5].protein)
-                val fat = str2Int(recommendation.lunch[6].fat)
-
-                Glide.with(requireContext())
-                    .load(recommendation.lunch[2].link)
-                    .into(binding.imgLunchR)
-                binding.txtLunchNameR.text = recommendation.lunch[1].name
-                binding.txtLunchCalR.text = "$cal kcal"
-
-                if (recommendation.lunch[8].eat) {
-//                    calTaken += cal
-//                    carbTaken += carb
-//                    protTaken += prot
-//                    fatTaken += fat
-
-                    Glide.with(requireContext())
-                        .load(recommendation.lunch[2].link)
-                        .into(binding.imgLunch)
-                    binding.txtLunchName.text = recommendation.lunch[1].name
-                    binding.txtLunchCal.text ="$cal kcal"
-                    binding.btnAddLunch.setImageResource(R.drawable.ic_round_edit_24)
-
-                    binding.cardLunchR.visibility = View.GONE
-                } else {
-                    Glide.with(requireContext()).clear(binding.imgLunch)
-                    binding.txtLunchName.text = getString(R.string.food_name)
-                    binding.txtLunchCal.text = getString(R.string.kcal)
-                    binding.btnAddLunch.setImageResource(R.drawable.ic_round_add_24)
-
-                    binding.cardLunchR.visibility = View.VISIBLE
-                }
-
-//                lLoaded = true
-            }
-
-            if (recommendation.dinner != null) {
-                val cal = str2Int(recommendation.dinner[4].calories)
-                val carb = str2Int(recommendation.dinner[7].carbohydrates)
-                val prot = str2Int(recommendation.dinner[5].protein)
-                val fat = str2Int(recommendation.dinner[6].fat)
-
-                Glide.with(requireContext())
-                    .load(recommendation.dinner[2].link)
-                    .into(binding.imgDinnerR)
-                binding.txtDinnerNameR.text = recommendation.dinner[1].name
-                binding.txtDinnerCalR.text = "$cal kcal"
-
-                if (recommendation.dinner[8].eat) {
-                    Glide.with(requireContext())
-                        .load(recommendation.dinner[2].link)
-                        .into(binding.imgDinner)
-                    binding.txtDinnerName.text = recommendation.dinner[1].name
-                    binding.txtDinnerCal.text = "$cal kcal"
-                    binding.btnAddDinner.setImageResource(R.drawable.ic_round_edit_24)
-
-                    binding.cardDinnerR.visibility = View.GONE
-                } else {
-                    Glide.with(requireContext()).clear(binding.imgDinner)
-                    binding.txtDinnerName.text = getString(R.string.food_name)
-                    binding.txtDinnerCal.text = getString(R.string.kcal)
-                    binding.btnAddDinner.setImageResource(R.drawable.ic_round_add_24)
-
-                    binding.cardDinnerR.visibility = View.VISIBLE
-                }
-
-//                dLoaded = true
-            }
-        }
-
-        viewModel.recMsg.observe(requireActivity()) { msg ->
-            Toast.makeText(requireContext(), msg, Toast.LENGTH_SHORT).show()
         }
     }
 
-    private fun updateEaten(id: String, date: String, type: String) {
-        val (day, month) = getDateMonthNum(date)
+    private fun calculateNutrition(id: String?, date: String) {
+        if (id != null) {
+            val (day, month) = getDateMonthNum(date)
 
-        showLoading()
+            showLoadingN()
 
-        viewModel.updateEat(id, day.toString(), month.toString(), type)
+            viewModel.calculateNutrition(id, day.toString())
 
-        viewModel.updateEaten.observe(viewLifecycleOwner) {
-            getRecommendation(id, date)
+            viewModel.nutritionTaken.observe(viewLifecycleOwner) { taken ->
+                if (taken != null) {
+                    binding.txtTakenCal.text = (str2Int(taken.calories)).toString()
+                    binding.txtTakenCarb.text = (str2Int(taken.carbohydrates)).toString()
+                    binding.txtTakenProt.text = (str2Int(taken.protein)).toString()
+                    binding.txtTakenFat.text = (str2Int(taken.fat)).toString()
+                }
+            }
         }
+    }
 
-        viewModel.updateMsg.observe(requireActivity()) {
-            Toast.makeText(requireContext(), it, Toast.LENGTH_SHORT).show()
+    private fun getRecommendation(id: String?, date: String) {
+        if (id != null) {
+            val (day, month) = getDateMonthNum(date)
+
+            showLoading()
+
+            viewModel.getRecommendation(id, day.toString(), month.toString())
+
+            viewModel.food.observe(viewLifecycleOwner) { recommendation ->
+                if (recommendation.breakfast != null) {
+                    val cal = str2Int(recommendation.breakfast[4].calories)
+                    val carb = str2Int(recommendation.breakfast[7].carbohydrates)
+                    val prot = str2Int(recommendation.breakfast[5].protein)
+                    val fat = str2Int(recommendation.breakfast[6].fat)
+
+                    Glide.with(requireContext())
+                        .load(recommendation.breakfast[2].link)
+                        .into(binding.imgBreakR)
+                    binding.txtBreakNameR.text = recommendation.breakfast[1].name
+                    binding.txtBreakCalR.text = "$cal kcal"
+
+                    if (recommendation.breakfast[8].eat) {
+//                    calTaken += cal
+//                    carbTaken += carb
+//                    protTaken += prot
+//                    fatTaken += fat
+
+                        Glide.with(requireContext())
+                            .load(recommendation.breakfast[2].link)
+                            .into(binding.imgBreak)
+                        binding.txtBreakName.text = recommendation.breakfast[1].name
+                        binding.txtBreakCal.text = "$cal kcal"
+                        binding.btnAddBreakfast.setImageResource(R.drawable.ic_round_edit_24)
+
+                        binding.cardBreakfastR.visibility = View.GONE
+                    } else {
+                        Glide.with(requireContext()).clear(binding.imgBreak)
+                        binding.txtBreakName.text = getString(R.string.food_name)
+                        binding.txtBreakCal.text = getString(R.string.kcal)
+                        binding.btnAddBreakfast.setImageResource(R.drawable.ic_round_add_24)
+
+                        binding.cardBreakfastR.visibility = View.VISIBLE
+                    }
+
+//                bLoaded = true
+                }
+
+                if (recommendation.lunch != null) {
+                    val cal = str2Int(recommendation.lunch[4].calories)
+                    val carb = str2Int(recommendation.lunch[7].carbohydrates)
+                    val prot = str2Int(recommendation.lunch[5].protein)
+                    val fat = str2Int(recommendation.lunch[6].fat)
+
+                    Glide.with(requireContext())
+                        .load(recommendation.lunch[2].link)
+                        .into(binding.imgLunchR)
+                    binding.txtLunchNameR.text = recommendation.lunch[1].name
+                    binding.txtLunchCalR.text = "$cal kcal"
+
+                    if (recommendation.lunch[8].eat) {
+//                    calTaken += cal
+//                    carbTaken += carb
+//                    protTaken += prot
+//                    fatTaken += fat
+
+                        Glide.with(requireContext())
+                            .load(recommendation.lunch[2].link)
+                            .into(binding.imgLunch)
+                        binding.txtLunchName.text = recommendation.lunch[1].name
+                        binding.txtLunchCal.text = "$cal kcal"
+                        binding.btnAddLunch.setImageResource(R.drawable.ic_round_edit_24)
+
+                        binding.cardLunchR.visibility = View.GONE
+                    } else {
+                        Glide.with(requireContext()).clear(binding.imgLunch)
+                        binding.txtLunchName.text = getString(R.string.food_name)
+                        binding.txtLunchCal.text = getString(R.string.kcal)
+                        binding.btnAddLunch.setImageResource(R.drawable.ic_round_add_24)
+
+                        binding.cardLunchR.visibility = View.VISIBLE
+                    }
+
+//                lLoaded = true
+                }
+
+                if (recommendation.dinner != null) {
+                    val cal = str2Int(recommendation.dinner[4].calories)
+                    val carb = str2Int(recommendation.dinner[7].carbohydrates)
+                    val prot = str2Int(recommendation.dinner[5].protein)
+                    val fat = str2Int(recommendation.dinner[6].fat)
+
+                    Glide.with(requireContext())
+                        .load(recommendation.dinner[2].link)
+                        .into(binding.imgDinnerR)
+                    binding.txtDinnerNameR.text = recommendation.dinner[1].name
+                    binding.txtDinnerCalR.text = "$cal kcal"
+
+                    if (recommendation.dinner[8].eat) {
+                        Glide.with(requireContext())
+                            .load(recommendation.dinner[2].link)
+                            .into(binding.imgDinner)
+                        binding.txtDinnerName.text = recommendation.dinner[1].name
+                        binding.txtDinnerCal.text = "$cal kcal"
+                        binding.btnAddDinner.setImageResource(R.drawable.ic_round_edit_24)
+
+                        binding.cardDinnerR.visibility = View.GONE
+                    } else {
+                        Glide.with(requireContext()).clear(binding.imgDinner)
+                        binding.txtDinnerName.text = getString(R.string.food_name)
+                        binding.txtDinnerCal.text = getString(R.string.kcal)
+                        binding.btnAddDinner.setImageResource(R.drawable.ic_round_add_24)
+
+                        binding.cardDinnerR.visibility = View.VISIBLE
+                    }
+
+//                dLoaded = true
+                }
+            }
+
+            viewModel.recMsg.observe(requireActivity()) { msg ->
+                Toast.makeText(requireContext(), msg, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun updateEaten(id: String?, date: String, type: String) {
+        if (id != null) {
+            val (day, month) = getDateMonthNum(date)
+
+            showLoading()
+
+            viewModel.updateEat(id, day.toString(), month.toString(), type)
+
+            viewModel.updateEaten.observe(viewLifecycleOwner) {
+                getRecommendation(id, date)
+            }
+
+            viewModel.updateMsg.observe(requireActivity()) {
+                Toast.makeText(requireContext(), it, Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
